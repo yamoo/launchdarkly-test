1d885c38a62dc0b0d45a22a3fa3514a9d01bb54b
Marty <yamoo@funac.jp>
2018-01-23 18:26:18 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var LaunchDarkly = require('ldclient-node');
+var secrets = require('./secrets.json');
+
+var client = LaunchDarkly.init(secrets.ldSkdkey);
+
+var user = {
+  key: 'bob@example.com'
+};
+
+client.once('ready', () => {
+  /**
+   * @param {string} Feature flag name
+   * @param {object} User data (key property is required)
+   * @param {boolean} Default value (used if errors such as a case that the flag is not found and a case that user.key is not specified)
+   */
+  client.variation('marty-flag', user, false, (err, showFeature) => {
+    if (showFeature) {
+      // application code to show the feature
+      console.log('[ ON ] Showing your feature to ' + user.key);
+    } else {
+      // the code to run if the feature is off
+      console.log('[ OFF ] Not showing your feature to ' + user.key);
+    }
+    client.flush(() => {
+      client.close();
+    });
+  });
+});
