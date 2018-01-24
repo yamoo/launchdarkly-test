660fef72d3e3e22fec81fe35c8d80336b6ecef8f
Marty <yamoo@funac.jp>
2018-01-24 13:33:18 +0900

    Changes to Promise way.

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ var user = {
   key: 'bob@example.com'
 };
 
-client.once('ready', () => {
+client.waitUntilReady().then(() => {
   /**
    * @param {string} Feature flag name
    * @param {object} User data (key property is required)
    * @param {boolean} Default value (used if errors such as a case that the flag is not found and a case that user.key is not specified)
    */
-  client.variation('marty-flag', user, false, (err, showFeature) => {
-    if (showFeature) {
-      // application code to show the feature
-      console.log('[ ON ] Showing your feature to ' + user.key);
-    } else {
-      // the code to run if the feature is off
-      console.log('[ OFF ] Not showing your feature to ' + user.key);
-    }
-    client.flush(() => {
-      client.close();
+  client.variation('marty-flag', user, false)
+    .then(showFeature => {
+      if (showFeature) {
+        // application code to show the feature
+        console.log('[ ON ] Showing your feature to ' + user.key);
+      } else {
+        // the code to run if the feature is off
+        console.log('[ OFF ] Not showing your feature to ' + user.key);
+      }
+
+      client.flush().then(() => {
+        client.close();
+      });
+    })
+    .catch(err => {
+      console.log('Error');
     });
-  });
 });
